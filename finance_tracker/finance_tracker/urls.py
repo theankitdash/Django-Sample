30f884c963dbff0f018853b5b461b162dfb6910e
Ankit Dash <ankitdash3037@gmail.com>
2024-02-17 18:42:45 +0530

    Django setup

--- finance_tracker/finance_tracker/urls.py
+++ finance_tracker/finance_tracker/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for finance_tracker project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('webpage/', include('webpage.urls')),
 ]
